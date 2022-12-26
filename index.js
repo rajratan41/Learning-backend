ddf2ba0a9964834e88c95696194014dc16440e21
Raj Ratan <raj808341@gmail.com>
2022-12-26 20:00:07 +0530

    updating index.js

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ app.get ("/", (req, res) => {
     res.status(200).send("Hello Raj Ratan");
 })
 
+app.get ("/insta", (req, res) => {
+    const insta = {
+        id: "raj_ratan04",
+        follower: 20,
+        follow : 10
+    }
+
+    res.status(200).json({insta})
+})
+
+app.get("/ac/twitter", (req, res) => {
+    const twitter = {
+        userName : "raj_ratan04",
+        followers : 34,
+        follows : 55,
+    };
+    res.status(200).json({ twitter })
+})
+
+app.get("/api/user/:token", (req, res) => {
+    console.log(req.params.token);
+    res.status(200).json({ param: req.params.token})
+})
+
+
+
+
 app.listen (port, () => {
     console.log("Server is running");
 })
